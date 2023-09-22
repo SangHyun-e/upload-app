dfee89bce6c5ec2672acaeb042590cd45e349c69
sanghyune <sanghyune_@naver.com>
2023-09-22 14:33:35 +0900

    별칭 오류해결

--- react-app/src/components/Upload.js
+++ react-app/src/components/Upload.js
@@ -11,16 +11,15 @@ import {
   TextField,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-
 const serverUrl = "http://localhost:3000";
 
 axios
-  .get("http://localhost:3000/api/data")
+  .get(`${serverUrl}/api/data`)
   .then((response) => {
-    console.log("서버 응답 데이터:", response.data);
+    console.log("서버 응답", response.data);
   })
   .catch((error) => {
-    console.error("서버 요청 에러:", error);
+    console.error("요청 에러", error);
   });
 
 class Upload extends Component {
@@ -28,84 +27,105 @@ class Upload extends Component {
     super(props);
 
     this.state = {
-      selectedFiles: [], // 여러 파일 저장할 배열
-      alias: "",
+      selectedFiles: [], // 선택한 파일 목록
+      aliases: [], // 파일 별 별칭 목록
+      errorMessage: "", // 오류 메시지
     };
   }
 
-  // 파일 입력 필드의 변경 이벤트 핸들러
+  // 파일 선택 시 실행되는 함수
   handleFileChange = (e) => {
-    // 선택한 파일을 상태에 업데이트
-    const files = Array.from(e.target.files);
-    this.setState((prevState) => ({
-      selectedFiles: [...prevState.selectedFiles, ...files],
-    }));
-  };
-  // 별칭 입력 필드의 변경 이벤트 핸들러
-  handleAliasChange = (e) => {
-    this.setState({ alias: e.target.value });
-  };
+    const selectedFiles = Array.from(e.target.files);
+    const { aliases } = this.state;
+    const newAliases = selectedFiles.map(() => "");
 
-  // 파일 삭제 버튼 클릭 시, 실행 될 함수
-  handleFileDelete = (file) => {
-    this.setState((prevState) => {
-      // 선택한 파일 목록에서 해당 파일을 제거
-      const updatedFiles = prevState.selectedFiles.filter((f) => f !== file);
-      return { selectedFiles: updatedFiles };
+    this.setState({
+      selectedFiles,
+      aliases: [...aliases, ...newAliases],
+      errorMessage: "",
     });
   };
 
-  // 업로드 버튼 클릭 시, 실행될 함수
+  // 별칭 입력 시 실행되는 함수
+  handleAliasChange = (e, index) => {
+    console.log("별칭 변경 - 인덱스:", index);
+    const { aliases } = this.state;
+    aliases[index] = e.target.value;
+    console.log("별칭 값:", e.target.value);
+    this.setState({ aliases });
+  };
+
+  // 파일 삭제 버튼 클릭 시 실행되는 함수
+  handleFileDelete = (index) => {
+    const { selectedFiles, aliases } = this.state;
+    selectedFiles.splice(index, 1); // 해당 파일 제거
+    aliases.splice(index, 1); // 해당 파일의 별칭 제거
+    this.setState({ selectedFiles, aliases });
+  };
+
+  // 업로드 버튼 클릭 시 실행되는 함수
   handleUpload = () => {
-    const { selectedFiles, alias } = this.state;
-    if (selectedFiles.length > 0) {
-      // 선택한 파일을 서버로 업로드하는 로직
-      const formData = new FormData();
-      selectedFiles.forEach((file, index) => {
-        formData.append("files", file);
-      });
+    const { selectedFiles, aliases } = this.state;
 
-      // 별칭도 formData에 추가
-      formData.append("alias", alias);
-
-      // Axios 사용해서 서버로 파일 업로드 요청
-      axios
-        .post(`${serverUrl}/upload-multiple`, formData, {
-          headers: {
-            "Content-Type": "multipart/form-data", // 파일 업로드 시 반드시 필요한 헤더
-          },
-        })
-        .then((response) => {
-          console.log("파일 업로드 성공!", response.data);
-          // 업로드 완료되면 상태 초기화
-          this.setState({ selectedFiles: [], alias: "" });
-        })
-        .catch((error) => {
-          // 업로드 중 에러가 발생한 경우 처리
-          console.error("파일 업로드 에러", error);
-        });
-    } else {
-      alert("파일을 선택하세요"); // 파일 선택 안할시 경고메세지
+    if (selectedFiles.length === 0) {
+      alert("파일을 선택하세요");
+      return;
     }
+
+    // 별칭을 입력하지 않은 경우 오류 메시지 표시
+    if (aliases.some((alias) => alias === "")) {
+      this.setState({
+        errorMessage: "모든 파일에 별칭을 입력해주세요",
+      });
+      return;
+    }
+
+    // FormData를 사용하여 서버로 업로드 요청 보냄
+    const formData = new FormData();
+    selectedFiles.forEach((file, index) => {
+      formData.append("files", file);
+      formData.append("aliases", aliases[index]);
+    });
+
+    axios
+      .post(`${serverUrl}/upload-multiple`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        console.log("파일 업로드 성공!", response.data);
+
+        // 업로드 성공 시 alert창 표시
+        alert("파일이 성공적으로 업로드 되었습니다.");
+
+        // 업로드 완료 후 상태 초기화
+        this.setState({
+          selectedFiles: [],
+          aliases: [],
+          errorMessage: "",
+        });
+      })
+      .catch((error) => {
+        console.error("파일 업로드 에러", error);
+      });
   };
 
   render() {
-    const { selectedFiles, alias } = this.state;
+    const { selectedFiles, aliases, errorMessage } = this.state;
 
     return (
       <div>
         <h2>파일 업로드</h2>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            {/* 파일 선택을 위한 입력 필드 (multiple 속성 추가) */}
             <input
               type="file"
               style={{ display: "none" }}
               onChange={this.handleFileChange}
               ref={(fileInput) => (this.fileInput = fileInput)}
-              multiple // 여러 파일 선택 가능하게 함
+              multiple
             />
-            {/* 업로드 버튼을 클릭하면 handleUpload 함수 실행 */}
             <Button
               variant="contained"
               color="primary"
@@ -115,45 +135,31 @@ class Upload extends Component {
             </Button>
           </Grid>
           <Grid item xs={12}>
-            {/* 별칭 입력 필드 */}
-            <TextField
-              label="별칭"
-              value={alias}
-              onChange={this.handleAliasChange}
-              fullWidth
-              variant="outlined"
-            />
+            <List>
+              {selectedFiles.map((file, index) => (
+                <ListItem key={index}>
+                  <ListItemText primary={file.name} />
+                  <TextField
+                    label="별칭"
+                    value={aliases[index]}
+                    onChange={(e) => this.handleAliasChange(e, index)}
+                    variant="outlined"
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton
+                      edge="end"
+                      aria-label="delete"
+                      onClick={() => this.handleFileDelete(index)}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
           </Grid>
         </Grid>
-        <br />
-        {/* 선택한 파일 목록 표시 */}
-        {selectedFiles.length > 0 && (
-          <List>
-            {selectedFiles.map((file, index) => (
-              <ListItem
-                key={index}
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <ListItemText primary={file.name} />
-                {/* 파일 삭제 버튼 */}
-                <ListItemSecondaryAction>
-                  <IconButton
-                    edge="end"
-                    aria-label="delete"
-                    onClick={() => this.handleFileDelete(file)}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-            ))}
-          </List>
-        )}
-        {/* 업로드 버튼 */}
+        {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
         <Button onClick={this.handleUpload} variant="contained" color="primary">
           업로드
         </Button>
